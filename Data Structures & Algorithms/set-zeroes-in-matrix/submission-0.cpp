b425eb64985c5baa0cac9b82a3f6b0f61b187a8a
Ricky Everest <118391742+Ricky-OdinEverest@users.noreply.github.com>
2026-05-13 14:57:21 -0700

    Add: set-zeroes-in-matrix - submission-0

---
+++ Data Structures & Algorithms/set-zeroes-in-matrix/submission-0.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix)
+    {
+        bool rowZero =  markDirty(matrix);
+        int ROWS = matrix.size(), COLS = matrix[0].size();
+
+        for (int r = 1; r < ROWS; r++)
+        {
+            for (int c = 1; c < COLS; c++)
+            {
+                if (matrix[0][c] == 0 || matrix[r][0] == 0)
+                {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+
+        if (matrix[0][0] == 0)
+        {
+            for (int r = 0; r < ROWS; r++)
+            {
+                matrix[r][0] = 0;
+            }
+        }
+
+        if (rowZero)
+        {
+            for (int c = 0; c < COLS; c++)
+            {
+                matrix[0][c] = 0;
+            }
+        }
+    }
+
+    bool markDirty(vector<vector<int>>& matrix)
+    {
+        bool firstRowDirty = false;
+        for(int r = 0; r < matrix.size(); r++)
+        {
+            for(int c = 0; c < matrix[0].size(); c++)
+            {
+                if(matrix[r][c] == 0)
+                {
+                    matrix[0][c] = 0;
+                    if (r > 0)
+                    {
+                        matrix[r][0] = 0;
+                    } 
+                    else 
+                    {
+                        firstRowDirty = true;
+                    }                
+                }
+            }
+        }
+
+        return firstRowDirty;
+    }
+
+};
