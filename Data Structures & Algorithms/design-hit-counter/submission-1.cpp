1f135f8a07f4d2cef26ae70a4501fab2946fb4c1
Ricky Everest <118391742+Ricky-OdinEverest@users.noreply.github.com>
2026-05-26 10:35:36 -0700

    Add: design-hit-counter - submission-1

---
+++ Data Structures & Algorithms/design-hit-counter/submission-1.cpp
@@ -0,0 +1,32 @@
+class HitCounter {
+public:
+    queue<int> hits;
+    HitCounter() {
+        
+    }
+    
+    void hit(int timestamp) {
+        hits.push(timestamp);
+    }
+    
+    int getHits(int timestamp) {
+    
+        while(!hits.empty())
+        {
+            
+            int val = timestamp - hits.front();
+            if(val >= 300) hits.pop();
+            else break;
+            
+        }
+
+        return hits.size();
+}
+};
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter* obj = new HitCounter();
+ * obj->hit(timestamp);
+ * int param_2 = obj->getHits(timestamp);
+ */
